ecc87f416721b7fc307b47858c973283edee2a5c
k <k@ks-iMac.local>
2016-02-03 20:55:59 +0000

    completed login for facebook and new firebase users

--- Lesson116-Firebase/DataService.swift
+++ Lesson116-Firebase/DataService.swift
@@ -11,15 +11,34 @@ import Firebase
 
 // singleton
 
+let URL_BASE = "https://lesson116-firebase.firebaseio.com"
+
 class DataService {
     
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://lesson116-firebase.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
     
     var REF_BASE: Firebase {
         
         return _REF_BASE
+    }
+    
+    var REF_POSTS: Firebase {
+        
+        return _REF_POSTS
+    }
+    
+    var REF_USERS: Firebase {
+        
+        return _REF_USERS
+    }
+    
+    func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
+        
+        REF_USERS.childByAppendingPath(uid).setValue(user)
     }
 }
 
