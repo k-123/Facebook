4c92567c6bbbc61f3b5127b921e5385de1ece065
k <k@ks-iMac.local>
2016-02-03 22:06:05 +0000

    successfully parsing firebase data

--- Lesson116-Firebase/FeedVC.swift
+++ Lesson116-Firebase/FeedVC.swift
@@ -7,29 +7,61 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController,
               UITableViewDelegate,
               UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
+          
+            // data is sent in the form of a snapshot by firebase
+            
+            self.posts = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+                    
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            //print(snapshot.value)
+            self.tableView.reloadData()
+        })
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        //print(post.postDescription)
         
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
